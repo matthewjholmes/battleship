c5083ba0cac7df86b8e1b0bd19b1d003b1de52c5
matthewjholmes <ilookup@gmail.com>
2021-07-17 12:54:37 -0600

    Finish #render in Board class; end of Iteration 2

--- lib/board.rb
+++ lib/board.rb
@@ -65,18 +65,19 @@ class Board
     y_axis = ("A".."D").to_a
     arr = []
     sentence = @cells.map do |cell|
-      cell[1].render(true)
+      " " + cell[1].render(render)
     end
-    lines = sentence.join.scan(/.{4}/)
-    lines.unshift(" 1234")
+
+    lines = sentence.join.scan(/.{8}/)
+    lines.unshift("  1 2 3 4")
     lines_spaces = lines.each do |line|
       line.each_char do |line|
         line.insert(-1, " ")
       end
-    end 
-    require "pry"; binding.pry
+    end
+
     board = lines.each do |line|
-       line.concat("\n")
+       line.concat(" \n")
 
     end
     board.zip(y_axis).join
